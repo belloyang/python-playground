86f9720782446168d86b95897610b7b038419570
Bing Yang <bingyang@nanometrics.ca>
2020-08-05 23:08:33 -0400

    update print syntax

--- main.py
+++ main.py
@@ -10,26 +10,26 @@ toolkit = EAW_ToolKit('')
 configFilename='config.txt'
 
 def listCommands():
-    print "Usage:  main.py [option] [params...]"
-    print " Option: "
-    print " -c or --config"
-    print " => parameters: [host]"
-    print " -l or --login"
-    print " => parameters: [acount] [password]"
-    print " -r or --register"
-    print " => parameters: [acount] [password]"
-    print " -ft or --find-target"
-    print " => parameters: none"
-    print " -rr or --register-range"
-    print " => parameters: [begin] [end]"
-    print " -bl or --brute-login"
-    print " => parameters: [account]"
+    print ("Usage:  main.py [option] [params...]")
+    print (" Option: ")
+    print (" -c or --config")
+    print (" => parameters: [host]")
+    print (" -l or --login")
+    print (" => parameters: [acount] [password]")
+    print (" -r or --register")
+    print (" => parameters: [acount] [password]")
+    print (" -ft or --find-target")
+    print (" => parameters: none")
+    print (" -rr or --register-range")
+    print (" => parameters: [begin] [end]")
+    print (" -bl or --brute-login")
+    print (" => parameters: [account]")
 
 
 def configHost(host):
     configFile=open(configFilename, 'w')
     configFile.write(host)
-    print 'Saving host:' + host
+    print ('Saving host:' + host)
 
 # Read config host
 def readConfig():
@@ -37,15 +37,15 @@ def readConfig():
         configFile=open(configFilename, 'r')
         host = configFile.readline()
         if host != '':
-            print 'Create toolkit for host:'+ host
+            print ('Create toolkit for host:'+ host)
             toolkit.host = host
         else:
-            print 'host is not configured:'
-            print "[-c or --config] [host]"
+            print ('host is not configured:')
+            print ("[-c or --config] [host]")
             exit(1)
     else:
-        print 'host is not configured:'
-        print "[-c or --config] [host]"
+        print ('host is not configured:')
+        print ("[-c or --config] [host]")
         exit(1)
 
 
@@ -57,21 +57,21 @@ else:
         listCommands()
     elif sys.argv[1]=='-c' or sys.argv[1]=='--config':
         if len(sys.argv) < 3 :
-            print "Missing arguments:"
-            print " [-c or --config] [host] " 
+            print ("Missing arguments:")
+            print (" [-c or --config] [host] ") 
         else:
             configHost(str(sys.argv[2]))
     elif sys.argv[1]=='-l' or sys.argv[1]=='--login':
         if len(sys.argv) < 4 :
-            print "Missing arguments:"
-            print " [-l or --login] [acount] [password]" 
+            print ("Missing arguments:")
+            print (" [-l or --login] [acount] [password]") 
         else:
             readConfig()
             toolkit._login(str(sys.argv[2]),str(sys.argv[3]))
     elif sys.argv[1]=='-r' or sys.argv[1]=='--register':
         if len(sys.argv) < 4 :
-            print "Missing arguments:"
-            print " [-r or --register] [acount] [password]" 
+            print ("Missing arguments:")
+            print (" [-r or --register] [acount] [password]") 
         else:
             readConfig()
             toolkit._register(str(sys.argv[2]),str(sys.argv[3]))
@@ -80,19 +80,19 @@ else:
         toolkit.findTargetAccount()
     elif sys.argv[1]=='-rr' or sys.argv[1]=='--register-range':
         if len(sys.argv) < 4 :
-            print "Missing arguments:"
-            print " [-rr or --register-range] [begin] [end]" 
+            print ("Missing arguments:")
+            print (" [-rr or --register-range] [begin] [end]") 
         else:
             readConfig()
             toolkit.registerRange(str(sys.argv[2]),str(sys.argv[3]))
     elif sys.argv[1]=='-bl' or sys.argv[1]=='--brute-login':
         if len(sys.argv) < 3 :
-            print "Missing arguments:"
-            print " [-rr or --register-range] [acount]" 
+            print ("Missing arguments:")
+            print (" [-rr or --register-range] [acount]") 
         else:
             readConfig()
             toolkit.bruteForceLogin(str(sys.argv[2]))
     else:
-        print 'Invalid Input'
+        print ('Invalid Input')
         listCommands()
 
